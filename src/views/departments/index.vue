195518e98b539b8c1f35fe1c2aabc5525287332a
lidoudoou <lidoudoou@gmail.com>
2022-11-10 11:50:55 +0800

    tree-tools component

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,69 +3,26 @@
     <div class="app-container" />
     <el-card class="tree-card">
       <!-- 用了一个行列布局 -->
-      <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-        <el-col>
-          <span>北京天通神策科技股份有限公司</span>
-        </el-col>
-        <el-col :span="4">
-          <el-row type="flex" justify="end">
-            <!-- 两个内容 -->
-            <el-col>负责人</el-col>
-            <el-col>
-              <!-- 下拉菜单 element -->
-              <el-dropdown>
-                <span>
-                  操作<i class="el-icon-arrow-down" />
-                </span>
-                <!-- 下拉菜单 -->
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+      <tree-tools :tree-node="company" />
       <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-        <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-          <el-col>
-            <!-- 左侧内容 -->
-            <span>{{ data.name }}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>{{ data.manager }}</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <!-- 内容 -->
-                  <span>操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 具名插槽 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <!-- 下拉选项 -->
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                    <el-dropdown-item>编辑部门</el-dropdown-item>
-                    <el-dropdown-item>删除部门</el-dropdown-item>
-
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
+        <tree-tools slot-scope="{ data }" :tree-node="data" />
       </el-tree>
     </el-card>
   </div>
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
+  components: {
+    treeTools
+  },
   data () {
     return {
+      company: {
+        name: '北京天通神策科技股份有限公司',
+        manager: '负责人'
+      },
       defaultProps: {
         label: 'name'
       },
