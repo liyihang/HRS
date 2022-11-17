f2832746f5325151f5111d9652d4f60117df38f6
lidoudoou <lidoudoou@gmail.com>
2022-11-17 16:05:37 +0800

    depts add validate

--- src/views/departments/components/add-depts.vue
+++ src/views/departments/components/add-depts.vue
@@ -2,18 +2,18 @@
   <!-- 弹层 -->
   <el-dialog title="新增部门" :visible="showDialog">
     <!-- 表单 -->
-    <el-form label-width="120px">
-      <el-form-item label="部门名称">
-        <el-input style="width:80%" />
+    <el-form label-width="120px" :model="formData" :rules="rules">
+      <el-form-item label="部门名称" prop="name">
+        <el-input v-model="formData.name" style="width:80%" />
       </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input style="width:80%" />
+      <el-form-item label="部门编码" prop="code">
+        <el-input v-model="formData.code" style="width:80%" />
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select style="width:80%" />
+      <el-form-item label="部门负责人" prop="manager">
+        <el-select v-model="formData.manager" style="width:80%" />
       </el-form-item>
-      <el-form-item label="部门介绍">
-        <el-input style="width:80%" type="textarea" :rows="3" />
+      <el-form-item label="部门介绍" prop="introduce">
+        <el-input v-model="formData.introduce" style="width:80%" type="textarea" :rows="3" />
       </el-form-item>
     </el-form>
     <!-- 确认消息 -->
@@ -32,6 +32,23 @@ export default {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        code: '',
+        manager: '',
+        introduce: ''
+      },
+      //   校验规则
+      rules: {
+        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' }, { min: 1, max: 50, message: '长度为1-50字符', trigger: 'blur' }],
+        code: [{ required: true, message: 'code不能为空', trigger: 'blur' }, { min: 1, max: 50, message: '长度为1-50字符', trigger: 'blur' }],
+        manager: [{ required: true, message: '负责人不能为空', trigger: 'blur' }],
+        introduce: [{ required: true, message: '介绍不能为空', trigger: 'blur' }, { min: 1, max: 50, message: '长度为1-50字符', trigger: 'blur' }]
+      }
     }
   }
 }
